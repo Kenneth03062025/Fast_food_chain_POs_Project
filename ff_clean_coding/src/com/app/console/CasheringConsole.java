2a1c9b68bec361657290bac866d9af60fb8e9a03
Natzmecs <167747023+Natzmecs@users.noreply.github.com>
2025-04-16 11:45:05 +0800

    Cashering Starts

--- ff_clean_coding/src/com/app/console/CasheringConsole.java
+++ ff_clean_coding/src/com/app/console/CasheringConsole.java
@@ -1,4 +1,40 @@
 package com.app.console;
 
+import java.util.Scanner;
+
 public class CasheringConsole {
+    private final static Scanner sc = new Scanner(System.in);
+
+    public static void getOpenCashering(){}
+
+    public static void displayCasheringDashboard(){
+        System.out.println("What do you want? ");
+        System.out.println("    [1] Create New Cashering");
+        System.out.println("    [2] Update Cashering");
+        System.out.println("    [3] View Stock");
+        System.out.println("    [4] Back to dashboard");
+        System.out.print("Enter your choice: ");
+        String choice = sc.nextLine();
+
+//        switch (choice) {
+//            case "1":
+//                // Create new cashering
+//                //w.inputCashering();
+//                break;
+//            case "2":
+//                //Update Cashering
+//                //e.inputUpdateCashering();
+//                break;
+//            case "3":
+//                // View Stock
+//                //r.inputViewStockCashering();
+//                break;
+//            case "4":
+//                //casheringDashboardBack();
+//                break;
+//            default:
+//                System.out.println("Invalid!");
+//                casheringDashboard();
+//        }
+    }
 }
